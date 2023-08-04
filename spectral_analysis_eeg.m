1fc113d0e461ecb3bf6ba00a96bd0b74a046024f
Hilman Asyrafi <52323792+hilman-108@users.noreply.github.com>
2023-08-04 10:06:50 +0700

    Update spectral_analysis_eeg.m

--- spectral_analysis_eeg.m
+++ spectral_analysis_eeg.m
@@ -95,7 +95,6 @@ ylabel('Power Spectral (\muV^{2})')
 xlim([0 60])
 grid on
 legend ('AF3','F7','F3','FC5','T7','P7','O1','O2','P8','T8','FC6','F4','F8','AF4')
-=
 
 figure;
 plot(frekuensi_eo, power_eyes_open);
