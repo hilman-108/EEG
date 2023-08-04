5daaf0b89d76854cbfc11ec89b140d113b1b0682
Hilman Asyrafi <52323792+hilman-108@users.noreply.github.com>
2023-08-04 09:54:43 +0700

    Update waveletica_eeg_14electrode.m

--- waveletica_eeg_14electrode.m
+++ waveletica_eeg_14electrode.m
@@ -15,6 +15,8 @@ for c=1:ch
 end
 channels={'AF3','F7','F3','FC5','T7','P7','O1','O2','P8','T8','FC6','F4','F8','AF4'};
 figure;
+
+%%source = http://www.mat.ucm.es/~vmakarov/Supplementary/wICAexample/TestExample.html%%
 PlotEEG(filtering, Fs, channels, 200, 'Filtering');
 
 [weight, sphere] = runica(filtering, 'verbose', 'off');
