353243eb0c28b012a94fa72b623ed2bcf274e4bf
Hilman Asyrafi <52323792+hilman-108@users.noreply.github.com>
2023-08-04 09:45:13 +0700

    Create waveletica_eeg_14electrode.m

---
+++ waveletica_eeg_14electrode.m
@@ -0,0 +1,27 @@
+clear; clc;
+eeglab;
+EEG = pop_biosig();
+[ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG);
+nama=ALLEEG.data;
+ss=nama(3:16,:);
+cc=ss(:,1:15360);
+
+data=rmbase(ss);
+
+Fs=128;
+ch=size(data,1);
+for c=1:ch
+    filtering(c,:)=eegfilt(data(c,:),128,1,45,0,64);
+end
+channels={'AF3','F7','F3','FC5','T7','P7','O1','O2','P8','T8','FC6','F4','F8','AF4'};
+figure;
+PlotEEG(filtering, Fs, channels, 200, 'Filtering');
+
+[weight, sphere] = runica(filtering, 'verbose', 'off');
+W = weight*sphere;
+icaEEG = W*filtering;
+
+[icaEEG2, opt]= RemoveStrongArtifacts(icaEEG, (1:14), 1.25, Fs);
+Data_wICA = inv(W)*icaEEG2;
+figure;
+PlotEEG(Data_wICA, Fs, channels, 200, 'wICA cleanned EEG');
